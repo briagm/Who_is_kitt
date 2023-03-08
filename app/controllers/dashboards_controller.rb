28caf812223d9a225370d1d04c8c3a904910fa38
Briag Martin <briag.martin@gmail.com>
2023-03-08 12:32:35 +0100

    complement

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,5 +2,12 @@ class DashboardsController < ApplicationController
 
   def show
     @user = current_user
+    @ranking = compute_users_ranking
+  end
+
+  private
+
+  def compute_users_ranking
+    User.order(score: :desc) # { user: user, score: 89} sorted by score: :desc
   end
 end
