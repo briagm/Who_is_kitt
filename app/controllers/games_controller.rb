de26a3d2fb32dd05e3284aad30322885aa886a6d
Briag Martin <briag.martin@gmail.com>
2023-03-08 12:03:48 +0100

    generate dashboard controller/view

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,6 @@
 class GamesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :show ]
+
   def show
     @game = Game.find(params[:id])
     # @player_one = @game.players[0]
