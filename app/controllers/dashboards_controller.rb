806101f50b2f2863415eb6b86fc3191e57580c0a
Charles Woehl <122440168+CharlesWoehl@users.noreply.github.com>
2023-03-10 16:28:57 +0100

    ajout end

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -9,15 +9,16 @@ class DashboardsController < ApplicationController
     @pending_game = @games.where(status: 'pending').last
   end
 
-
   private
 
   def user_ranking
-     User.select(:id, :score).order(score: :desc).each_with_index do |user, index|
-  puts "Classement #{index + 1} : utilisateur #{user.id} avec un score de #{user.score}"
+    User.select(:id, :score).order(score: :desc).each_with_index do |user, index|
+    puts "Classement #{index + 1} : utilisateur #{user.id} avec un score de #{user.score}"
+    end
+  end
+  def compute_users_ranking
+      User.order(score: :desc) # { user: user, score: 89} sorted by score: :desc
   end
 
-  def compute_users_ranking
-    User.order(score: :desc) # { user: user, score: 89} sorted by score: :desc
-  end
+
 end
